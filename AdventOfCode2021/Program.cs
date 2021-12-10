c986b4552d6c7d93b4a43a89e03576ca7b937570
Vilius Vaicekauskas <vilius.vaicekauskas@outlook.com>
2021-12-10 21:20:23 +0200

    Day2 - Dive - Part 1

--- AdventOfCode2021/Program.cs
+++ AdventOfCode2021/Program.cs
@@ -12,6 +12,10 @@ namespace AdventOfCode2021
 			day1SonarSweep.SetInputData();
 			Console.WriteLine(day1SonarSweep.CalculateIncreasedMeasurements());
 			Console.WriteLine(day1SonarSweep.CalculateIncreasedGroupMeasurements());
+
+			var day2Dive = new Day2Dive();
+			day2Dive.SetInputData();
+			Console.WriteLine(day2Dive.MultiplyHorizontalPositionAndDepth());
 			
 		}
 	}
