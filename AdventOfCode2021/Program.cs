9ff6ef6d624075c0ca0508fbfaea092b200ef75b
Vilius Vaicekauskas <vilius.vaicekauskas@outlook.com>
2021-12-18 15:21:44 +0200

    Day 3 - part 1

--- AdventOfCode2021/Program.cs
+++ AdventOfCode2021/Program.cs
@@ -13,9 +13,15 @@ namespace AdventOfCode2021
 			Console.WriteLine(day1SonarSweep.CalculateIncreasedMeasurements());
 			Console.WriteLine(day1SonarSweep.CalculateIncreasedGroupMeasurements());
 
+			// Day 2: Dive
 			var day2Dive = new Day2Dive();
 			day2Dive.SetInputData();
 			Console.WriteLine(day2Dive.MultiplyHorizontalPositionAndDepth());
+
+			// Day 3: Binary Diagnostic
+			var day3BinaryDiagnostic = new Day3BinaryDiagnostic();
+			day3BinaryDiagnostic.SetInputData();
+			Console.WriteLine(day3BinaryDiagnostic.CalculatePowerConsumption());
 			
 		}
 	}
